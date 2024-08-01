1ed5d33e3882a07829d449dfd5c68c2926d2ce1e
Lucas Rios <lucasfaesa@gmail.com>
2024-08-01 15:32:08 -0300

    camera animation (offset upwards) at the end of level

--- Assets/_Game/GameScene/Scripts/Camera/CameraController.cs
+++ Assets/_Game/GameScene/Scripts/Camera/CameraController.cs
@@ -9,17 +9,20 @@ public class CameraController : MonoBehaviour
     [Header("SOs")]
     [SerializeField] private CameraEventsChannelSO cameraEventsChannel;
     [SerializeField] private LevelEventsChannelSO levelEventsChannel;
+    [SerializeField] private AnimationsEventChannelSO animationsEventChannel;
     [Header("Components")]
     [SerializeField] private Camera mainCamera;
 
     private void OnEnable()
     {
         levelEventsChannel.NewLevelLoaded += OnNewLevelLoaded;
+        animationsEventChannel.FinishLevelPostProcessingAnimationsEnded += OffsetCameraUp;
     }
 
     private void OnDisable()
     {
         levelEventsChannel.NewLevelLoaded -= OnNewLevelLoaded;
+        animationsEventChannel.FinishLevelPostProcessingAnimationsEnded -= OffsetCameraUp;
     }
 
     private void OnNewLevelLoaded(Level level)
@@ -29,6 +32,10 @@ public class CameraController : MonoBehaviour
         DOTween.To(x => mainCamera.orthographicSize = x, mainCamera.orthographicSize,
             level.GetLevelSettings().PreferredCameraSize, 2f).SetEase(Ease.OutBack)
             .OnComplete(cameraEventsChannel.OnCameraSizeAnimationEnded);
+    }
 
+    private void OffsetCameraUp()
+    {
+        Sequence offset = DOTween.Sequence().Append(mainCamera.transform.DOMoveY(-0.89f, 0.5f).SetEase(Ease.InOutSine)).PrependInterval(0.3f);
     }
 }
